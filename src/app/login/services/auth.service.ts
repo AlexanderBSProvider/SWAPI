20249224466f23737ab7e01ce2864c398aad5d86
Viktoriia Serhieieva <viktoriia@codinglab.com>
2022-01-10 14:30:20 +0200

    minor fixes

--- src/app/login/services/auth.service.ts
+++ src/app/login/services/auth.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
-import {catchError, Observable, of} from 'rxjs';
-import { tap } from 'rxjs/operators';
 import { Router } from "@angular/router";
 
 @Injectable({
@@ -27,17 +25,8 @@ export class AuthService {
 
   constructor(private http: HttpClient, private router: Router) { }
 
-  // register(email: string, password: string) {
-  //   this.http.post(this.api + 'users/login', {email, password}).subscribe((response:any) => {
-  //     console.log(response)
-  //     this.router.navigate(['']);
-  //     sessionStorage.setItem('auth_token', response.token);
-  //   });
-  // }
-
   register(user:any) {
     this.http.post(this.api + 'users/registration', user).subscribe((response:any) => {
-      console.log(response,'res')
       this.router.navigate(['']);
       sessionStorage.setItem('auth_token', response.token);
     });
@@ -46,9 +35,7 @@ export class AuthService {
   login(user:any) {
     return this.http.post(this.api + 'users/login', user).subscribe((response:any) => {
       this.router.navigate(['dashboard']);
-      console.log(response,'res')
       sessionStorage.setItem('auth_token', response.token);
-      console.log(sessionStorage, 'sessss')
     }
       );
   }
@@ -60,7 +47,6 @@ export class AuthService {
   setUserRegister(email:string, password:string){
     this.userRegister.email = email;
     this.userRegister.password = password;
-    // console.log(JSON.stringify(this.userRegister))
     this.register(this.userRegister);
   }
 
